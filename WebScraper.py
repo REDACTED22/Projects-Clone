9d70e3bb3bbc853e259d0b3e428316e905393fea
Alexander McGreevy <150959413+REDACTED22@users.noreply.github.com>
2024-12-04 16:31:28 -0500

    improvements

--- WebScraper.py
+++ WebScraper.py
@@ -12,9 +12,19 @@ print(r)
 # Parsing the HTML
 soup = BeautifulSoup(r.content, 'html.parser')
 soup=soup.prettify()
-#print(soup)
-for line in soup:
-    for word in line:
-        # Check if the line starts with "python" (case-sensitive)
-        if word.lower()=='theft':
-            print(line.strip())
+file = open("Web.txt", "w")
+file.write(soup)
+
+
+def get_lines_containing_word(file_path, word):
+    matching_lines = []
+    with open('Web.txt', "r") as file:
+        for line in file:
+            # Check if the word is in the line
+            if word in line:
+                matching_lines.append(line.strip())  # Add to the list without extra whitespace
+    return matching_lines
+
+
+
+file.close
