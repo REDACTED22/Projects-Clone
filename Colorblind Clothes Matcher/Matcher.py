8df72e5d4f15b48d0a616001a476d8c931ef35d7
Alexander McGreevy <150959413+REDACTED22@users.noreply.github.com>
2025-01-14 14:57:15 -0500

    Saving my progress
    Figured out how to graph images to a histogram for color detection

--- Colorblind Clothes Matcher/Matcher.py
+++ Colorblind Clothes Matcher/Matcher.py
@@ -1,5 +1,18 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+
+
 
 #Converts image to HSV color space
 #cv2.cvtColor
+#Next need to use numpy to display the chart and match different photos together based on color
+
+
+shirt="/Users/alexandermcgreevy/Documents/GitHub/Projects-Clone/Colorblind Clothes Matcher/BlueShirt.jpg"
+
+img = cv2.imread(shirt)
+
+histogram=cv2.calcHist([img],[0],None,[256],[0,256])
+plt.plot(histogram)
+plt.show()
