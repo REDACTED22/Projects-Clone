79b42cfee885a2e05c220aabf20976cb9afcb1dc
Alexander McGreevy <150959413+REDACTED22@users.noreply.github.com>
2025-01-24 09:34:06 -0500

    Started working on a Swift App to untegrate with the clothing matcher

---
+++ ClothesMatcher/ClothesMatcher/ClothesMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesMatcherApp.swift
+//  ClothesMatcher
+//
+//  Created by Alexander McGreevy on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ClothesMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothesMatcher/ClothesMatcherTests/ClothesMatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesMatcherTests.swift
+//  ClothesMatcherTests
+//
+//  Created by Alexander McGreevy on 1/23/25.
+//
+
+import Testing
+@testable import ClothesMatcher
+
+struct ClothesMatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
